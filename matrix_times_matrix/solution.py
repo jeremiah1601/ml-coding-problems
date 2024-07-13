215f9a1e029233d2d39038133c10203834e2b9c1
Jeremiah Barte <jeremiahbarte@gmail.com>
2024-07-13 22:32:12 +0100

    final commit of da night

---
+++ matrix_times_matrix/solution.py
@@ -0,0 +1,18 @@
+def matrixmul(a:list[list[int|float]],
+              b:list[list[int|float]])-> list[list[int|float]]: 
+ return c
+
+
+def matrix_vector_mul(a: list[list[int]],
+             b: list[int]) -> list[int]:
+    # for row in rows do vector multiplication or return -1 if cant
+    res = []
+    for i in range(len(a)):
+        if len(a[i]) != len(b):
+            raise ValueError("Dimension's dont match")
+        res.append(vector_product(a[i], b))   
+    return res
+        
+
+def vector_product(v_1: list[int], v_2: list[int]) -> int:
+        return sum([v_1[i] * v_2[i] for i in range(len(v_1))])
